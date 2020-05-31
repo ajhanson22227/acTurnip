596c1562565b5a76840478cefa6dd931255e2a3c
adam <ajhanson22227@gmail.com>
2020-05-31 15:59:11 -0500

    Set up firebase, and some pngs

--- lib/TurnipTips/tip_calculator.dart
+++ lib/TurnipTips/tip_calculator.dart
@@ -51,9 +51,9 @@ class _TipCalculatorAppState extends State<TipCalculator>{
 
     //users total tips
     userOneTip = tip * userOneTotal;
-    userOneTip % 10000 != 0 ? userOneRoundedTip = (10000 - (userOneTip % 10000) + userOneTip) : userOneRoundedTip = userOneTip;
+    userOneTip % 1000 != 0 ? userOneRoundedTip = (1000 - (userOneTip % 1000) + userOneTip) : userOneRoundedTip = userOneTip;
     userTwoTip = tip * userTwoTotal;
-    userTwoTip % 10000 != 0 ? userTwoRoundedTip = (10000 - (userTwoTip % 10000) + userTwoTip) : userTwoRoundedTip = userTwoTip;
+    userTwoTip % 1000 != 0 ? userTwoRoundedTip = (1000 - (userTwoTip % 1000) + userTwoTip) : userTwoRoundedTip = userTwoTip;
     
     totalTip = userOneRoundedTip + userTwoRoundedTip;
 
@@ -71,104 +71,120 @@ class _TipCalculatorAppState extends State<TipCalculator>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomPadding: false,
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
- 
-            //userOne textfield
-            Container(
-            width: 280,
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-                controller: userOneController,
-                autocorrect: true,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Hoekills',
-                  prefixIcon: Icon(Icons.person, color: Colors.red,),
-                ),
-              )
-            ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
 
-            //userTwo textfield
-            Container(
-            width: 280,
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-                controller: userTwoController,
-                autocorrect: true,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Ticest',
-                  prefixIcon: Icon(Icons.person, color: Colors.red,),
-                ),
-              )
-            ),
+              //userOne textfield
+              Container(
+              width: 280,
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                  controller: userOneController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Hoekills',
+                    icon: Container(
+                      height: 20,
+                      width: 20,
+                      child: Image.asset('assets/hoekills.png',
+                      ),
+                    ),
+                  ),
+                )
+              ),
 
-            //turnip price textfield
-            Container(
-            width: 280,
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-                controller: turnipPriceController,
-                autocorrect: true,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'dolla dolla bill yall',
-                  prefixIcon: Icon(Icons.monetization_on, color: Colors.green,),
-                ),
-              )
-            ),
+              //userTwo textfield
+              Container(
+              width: 280,
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                  controller: userTwoController,
+                  autocorrect: true,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Ticest',
+                    icon: Container(
+                      height: 20,
+                      width: 20,
+                      child: Image.asset('assets/coolboy.png'),
+                    ),
+                  ),
+                )
+              ),
 
-            //tip percentage dropdown
-            DropdownButton<String>(
-              value: dropdownValue,
-              icon: Icon(Icons.arrow_drop_down),
+              //turnip price textfield
+              Container(
+              width: 280,
+              padding: EdgeInsets.all(10.0),
 
-              onChanged: (String newValue){
-                setState(() {
-                  dropdownValue = newValue;
-                });
-              },
+              child: TextField(
+                  controller: turnipPriceController,
+                  autocorrect: true,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'dolla dolla bill yall',
+                    icon: Container(
+                      height: 25,
+                      width: 25,
+                      child: Image.asset('assets/turnip.png')
+                    ),
+                  ),
+                )
+              ),
 
-              items: <String>['10','15','20']
-                .map<DropdownMenuItem<String>>((String value){
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                })
-              .toList(),
-            ),
+              //tip percentage dropdown
+              DropdownButton<String>(
+                value: dropdownValue,
+                icon: Icon(Icons.arrow_drop_down),
 
-            RaisedButton(
-              onPressed: getTextInputData,
-              color: Color(0xffFF1744),
-              textColor: Colors.white,
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              child: Text('Click Here To Math'),
-            ),
- 
-            Padding(
-            padding: EdgeInsets.all(8.0),
-            child :
-            Text("Total Tip is = $tipTotalString", style: TextStyle(fontSize: 20))
-            ),
+                onChanged: (String newValue){
+                  setState(() {
+                    dropdownValue = newValue;
+                  });
+                },
 
-            Padding(
-            padding: EdgeInsets.all(8.0),
-            child :
-            Text("Hoe total = $userOneTotalReturn", style: TextStyle(fontSize: 20))
-            ),
+                items: <String>['10','15','20']
+                  .map<DropdownMenuItem<String>>((String value){
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  })
+                .toList(),
+              ),
 
-            Padding(
-            padding: EdgeInsets.all(8.0),
-            child :
-            Text("Tice total = $userTwoTotalReturn", style: TextStyle(fontSize: 20))
-            ),
- 
-          ],
+              RaisedButton(
+                onPressed: getTextInputData,
+                color: Color(0xffFF1744),
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: Text('Click Here To Math'),
+              ),
+  
+              Padding(
+              padding: EdgeInsets.all(8.0),
+              child :
+              Text("Total Tip is = $tipTotalString", style: TextStyle(fontSize: 20))
+              ),
+
+              Padding(
+              padding: EdgeInsets.all(8.0),
+              child :
+              Text("Hoe total = $userOneTotalReturn", style: TextStyle(fontSize: 20))
+              ),
+
+              Padding(
+              padding: EdgeInsets.all(8.0),
+              child :
+              Text("Tice total = $userTwoTotalReturn", style: TextStyle(fontSize: 20))
+              ),
+  
+            ],
+          ),
         ),
       )
     );
